a2f89b8819d2704c0800b6c5e0b8a3b1883a478a
Munteanu Petrisor <petrisor.munteanu@activemall.com>
2018-10-26 13:52:25 +0300

    Add UserSummary payload

---
+++ src/main/java/mp27/security/starter/payload/UserSummary.java
@@ -0,0 +1,14 @@
+package mp27.security.starter.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserSummary {
+    private Long id;
+    private String username;
+    private String name;
+}
